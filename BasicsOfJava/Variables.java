60dbd70f4f97ae3d7aa5adc2f3df3f4817f70973
rksorry7255tu <rk7255053708@gmail.com>
2024-10-27 06:49:55 +0530

    basic of variables code

---
+++ BasicsOfJava/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String []args){
+        String name="Ravi";
+        String neighbour="monu";
+        int age=19;
+        String friend=neighbour;
+        System.out.println(name);
+        System.out.println(friend);
+
+    }
+}
