3e197d170a69eef6fb7cffde99969a6dc25853cc
rksorry7255tu <rk7255053708@gmail.com>
2024-10-28 07:48:59 +0530

    casting concepts

---
+++ BasicsOfJava/typeCasting.java
@@ -0,0 +1,17 @@
+public class typeCasting {
+    public static void main(String[] args) {
+
+
+        //implicit casting
+        double a=100.0;
+        double b=a+10;
+        System.out.println(b);
+
+        //explicit casting
+        int c=100;
+        int d=c+(int)10.2;
+        System.out.println(d);
+
+        
+    }
+}
