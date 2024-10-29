1b031a3696040fc1f740120613a93d4ac993c106
rksorry7255tu <rk7255053708@gmail.com>
2024-10-29 21:15:20 +0530

    loop revisions

---
+++ BasicsOfJava/Loop.java
@@ -0,0 +1,22 @@
+public class Loop {
+    public static void main(String[] args) {
+        //loop
+        for(int i=0;i<111;i++){
+            System.out.println("Hello world "+i);
+        }
+
+        //while
+        int i=0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+
+        //do-while
+        int j=0;
+        do{
+            System.out.println("do="+j);
+            j++;
+        }while(j<10);
+    }
+}
