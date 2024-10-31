5cd379ba02cf57aef28f9e8fda75fcc64eb0244b
rksorry7255tu <rk7255053708@gmail.com>
2024-10-31 08:19:23 +0530

    fib problem solved

---
+++ BasicsOfJava/nthFibonacciNo.java
@@ -0,0 +1,14 @@
+//return the nth fibonacci sequence no
+
+public class nthFibonacciNo {
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+    }
+    public static int fib(int n){
+        if(n==1||n==2){
+            return 1;
+        }
+            return fib(n-1)+fib(n-2);
+        
+    }
+}
