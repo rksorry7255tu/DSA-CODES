1d803ae5c7bd9c4b77ac475d276a3cc9bdea0b7c
rksorry7255tu <rk7255053708@gmail.com>
2024-11-01 21:24:13 +0530

    revision

--- BasicsOfJava/pattern4.java
+++ BasicsOfJava/pattern4.java
@@ -2,9 +2,9 @@ public class pattern4 {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
             for(int j=1;j<=5-i;j++){
-                System.out.print("* ");
+                System.out.print("*");
             }
-            System.out.println(" ");
+            System.out.println("");
         }
     }
 }
