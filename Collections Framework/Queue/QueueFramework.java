e2a7e9ae3fff26cbd3718575723bfb48ca1a2acb
rksorry7255tu <rk7255053708@gmail.com>
2024-11-06 08:20:20 +0530

    commited

---
+++ Collections Framework/Queue/QueueFramework.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class QueueFramework {
+    public static void main(String[] args) {
+        Queue <Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        // System.out.println(q.peek());
+        // System.out.println(q.size());
+        System.out.println(q);
+        System.out.println(q.size());
+        
+    }
+}
