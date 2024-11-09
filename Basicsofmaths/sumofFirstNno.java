1c6228cf3c51b538e0bdc13959756945a684ebc7
rksorry7255tu <rk7255053708@gmail.com>
2024-11-09 09:15:12 +0530

    commited

---
+++ Basicsofmaths/sumofFirstNno.java
@@ -0,0 +1,17 @@
+public class sumofFirstNno {
+    public static void main(String[] args) {
+        int n=4;
+       System.out.println(sum(n));
+      
+       
+    }
+    public static int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        
+        return n+sum(n-1);
+        
+    }
+ 
+}
