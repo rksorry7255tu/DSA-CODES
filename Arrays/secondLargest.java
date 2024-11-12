6faec798700c074ab1347df953c8ef3518f27684
rksorry7255tu <rk7255053708@gmail.com>
2024-11-13 00:54:43 +0530

    commited

--- Arrays/secondLargest.java
+++ Arrays/secondLargest.java
@@ -1,18 +1,29 @@
 
 class secondLargest {
     public static void main(String[] args) {
-        int arr[] = { 12, 35, 1, 10, 34, 1 };
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>=arr[j]){
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+        // int arr[] = { 12, 35, 1, 10, 34, 1 };
+        int arr[] = { 10, 10 };
+       
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[i] > arr[j]) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
                 }
             }
-        }
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
+
+            int secondlarge = 0;
+            for (int i = arr.length - 2; i >= 0; i--) {
+                if (arr[i] != arr[arr.length - 1]) {
+                    secondlarge = arr[i];
+                    break;
+                }else{
+                    secondlarge=-1;
+                }
+            }
+            System.out.println(secondlarge);
+      
     }
 }
