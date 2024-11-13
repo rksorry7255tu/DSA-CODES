a49f39b699856882e851162cb7d7a6589bcd2278
rksorry7255tu <rk7255053708@gmail.com>
2024-11-14 05:01:51 +0530

    commited

---
+++ Arrays/check1.java
@@ -0,0 +1,23 @@
+public class check1 {
+    public static void main(String[] args) {
+        //int arr[]={2,1,3,4};
+        int arr[]={4,5,6,3,2};
+        // int index=0;
+        // boolean result=true;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]>arr[(i+1)%arr.length]){
+        //         index++;
+        //     }
+        //     if(index>1){
+        //         //System.out.println("false");
+        //         result=false;
+        //     }
+        // }
+        // System.out.println(result);
+
+       System.out.println(1%5);
+       System.out.println(1/5);
+       
+        
+    }
+}
---
+++ Arrays/removeDuplicate.java
@@ -0,0 +1,22 @@
+public class removeDuplicate {
+    public static void main(String[] args) {
+        int arr[]={0,0,1,1,1,2,2,3,3,4};
+       int  count=1;
+        int i=0;
+       while(i<arr.length-1){
+        if(arr.length==1){
+            count=1;
+            break;
+        }
+        if(arr[i]==arr[i+1]){
+            i++;
+        }else{
+            count++;
+            i++;
+        }
+
+       }
+       System.out.println(count);
+          
+    }
+}
