5f58fadeeb77b4925225f6937ea5704826ff67d4
rksorry7255tu <rk7255053708@gmail.com>
2024-11-15 17:28:08 +0530

    commited

---
+++ Arrays/maxConsicutiveOnce.java
@@ -0,0 +1,16 @@
+public class maxConsicutiveOnce {
+    public static void main(String[] args) {
+        int nums[]={1,1,0,1,1,1};
+        int count=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=1){
+                count=0;
+            }else{
+                count++;
+                max=Math.max(count, max);
+            }
+        }
+        System.out.println(max);
+    }
+}
