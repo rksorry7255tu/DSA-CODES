6dbfff0b246e65ef1913922dc99298fa73e63c83
rksorry7255tu <rk7255053708@gmail.com>
2024-11-16 07:34:02 +0530

    commited

---
+++ Arrays/maxSubArray.java
@@ -0,0 +1,22 @@
+public class maxSubArray {
+    public static void main(String[] args) {
+        int arr[]={10, 5, 2, 7, 1, 9};
+       int k=15;
+       int  length=0;
+       for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                int sum=0;
+                for(int l=i;l<j;l++){
+                    sum=sum+arr[l];
+                }
+                if(sum==k){
+                    length=Math.max(length,j-i+1);
+                }
+            }
+       }
+       System.out.println(length);
+
+
+       
+    }
+}
---
+++ Arrays/maxSubArrayOptimize.java
@@ -0,0 +1,26 @@
+public class maxSubArrayOptimize {
+    public static void main(String[] args) {
+        int arr[] = { 1 ,4 ,3 ,3 ,5 ,5};
+        int k = 16;
+
+        int i = 0;
+        int j = 0;
+        int sum = arr[0];
+        int length=0;
+        while (i < arr.length) {
+            while(j<=i&&sum>k){
+                sum=sum-arr[j];
+                j++;
+            }
+            if(sum==k){
+                length=Math.max(length,i-j+1);
+            }
+            i++;
+            if(i<arr.length){
+                sum=sum+arr[i];
+            }
+            
+        }
+        System.out.println(length);
+    }
+}
