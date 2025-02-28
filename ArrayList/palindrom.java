ea13de1f646a7ab53a7d04dc99dc0764aa2c97a4
rksorry7255tu <rk7255053708@gmail.com>
2025-02-28 22:35:51 +0530

    commited

---
+++ ArrayList/palindrom.java
@@ -0,0 +1,18 @@
+public class palindrom {
+  public static void main(String[] args) {
+    int x = 121;
+    int reverse = 0;
+    int n = x;
+    while (n > 0) {
+      int digit = n % 10;
+      n = n / 10;
+      reverse = reverse * 10 + digit;
+
+    }
+    if (reverse == x) {
+      System.out.println("palindrom");
+    } else {
+      System.out.println("notpalindrom");
+    }
+  }
+}
